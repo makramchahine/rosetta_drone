4b20e9f77a6d24e7be1a3ff3ad2ef03cdd1670c2
JosephFeld <josephfeld2019@gmail.com>
2021-08-11 14:17:53 -0400

    updates for image mode and csv times

--- video_compression/scripts/ffio_example.py
+++ video_compression/scripts/ffio_example.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 
+import time as TimerTime
 import numpy as np
 import rospy
 from sensor_msgs.msg import Image, Joy
@@ -14,18 +15,24 @@ import subprocess
 
 import std_msgs.msg
 
+import os
+
 class VideoCompressionNode():
 
     def __init__(self): 
         rospy.init_node('video_compression_node')
 
-	self.path = "/home/dji/flash/"
+        self.path = "/home/dji/flash/"
 
-	self.delay_secs = 5
-	self.last_change_time = rospy.get_time() - self.delay_secs
-	self.last_input = 8000
+        self.image_mode = False
 
-	self.last_message = "starting up"
+        self.path_appendix = ""
+
+        self.delay_secs = 5
+        self.last_change_time = rospy.get_time() - self.delay_secs
+        self.last_input = 8000
+
+        self.last_message = "starting up"
 
         self.crf = 10
         self.compression_preset = 'ultrafast'
@@ -37,16 +44,19 @@ class VideoCompressionNode():
         self.video_open = False
         self.close_video = True
 
-        self.width = 1280
-        self.height = 720
+        #self.width = 1280
+        #self.height = 720
+
+        self.width = 640
+        self.height = 360
 
         self.frameid = 0
 
         rospy.Subscriber('dji_osdk_ros/main_camera_images', Image, self.image_cb)
         rospy.Subscriber('dji_osdk_ros/rc', Joy, self.joy_cb)
 
-	#start camera stream
-	rospy.wait_for_service('/setup_camera_stream')
+        #start camera stream
+        rospy.wait_for_service('/setup_camera_stream')
         try:
             video_starter = rospy.ServiceProxy('/setup_camera_stream', SetupCameraStream)
 	    resp1 = video_starter(1, 1)
@@ -61,82 +71,110 @@ class VideoCompressionNode():
         rospy.spin()
 
     def image_cb(self, msg):
-	h = std_msgs.msg.Header()
-	h.seq = 0
-	h.stamp = rospy.Time.now()
-	h.frame_id = ''
-        rospy.wait_for_service('/gimbal_task_control')
-	gimbalctrl = rospy.ServiceProxy('/gimbal_task_control', GimbalAction)	
+        h = std_msgs.msg.Header()
+        h.seq = 0
+        h.stamp = rospy.Time.now()
+        h.frame_id = ''
         if not self.video_open and not self.close_video:
 
-	    resp2 = gimbalctrl(h, False, 0,  0,  90.0,  0.0, 0.0, 0.5)
+            rospy.wait_for_service('/gimbal_task_control')
+            gimbalctrl = rospy.ServiceProxy('/gimbal_task_control', GimbalAction)	
+ 
+
+            resp2 = gimbalctrl(h, False, 0,  0,  90.0,  0.0, 0.0, 0)
+
+
+            #resp2 = gimbalctrl(h, False, 0,  0,  -90.0,  0.0, 0.0, 0)
+            resp2 = gimbalctrl(h, True, 0,  0, 0.0,  0.0, 0.0, 0)
+
+            print("starting recording")
+            
             subprocess.Popen(["sh","/home/dji/catkin_ws/src/rosbagg/src/bagstart.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-	    print("starting recording")
-
-	    resp2 = gimbalctrl(h, False, 0,  0,  -90.0,  0.0, 0.0, 0.5)
-	    resp2 = gimbalctrl(h, True, 0,  0, 0.0,  0.0, 0.0, 0.5)
-            rostime = rospy.Time.now()
+            rostime = msg.header.stamp#rospy.Time.now()
             time = rostime.secs + rostime.nsecs * 1e-9
             self.video_start_time = time
-            self.video_times = []
-            self.writer = ffio.FFWriter(self.path+'%.2f.mp4' % time, self.height, self.width, crf=self.crf, preset=self.compression_preset, verbose=True)
+
+
+            if not self.image_mode:
+                self.video_times = []
+                self.writer = ffio.FFWriter(self.path+'%.2f.mp4' % time, self.height, self.width, crf=self.crf, preset=self.compression_preset, verbose=True)
+            else:
+                self.path_appendix = '%f' % time
+                os.mkdir(os.path.join(self.path, self.path_appendix))
             self.video_open = True
         elif self.video_open and self.close_video:
             print("ending recording")
 
+
+            if not self.image_mode:
+                self.writer.close()
+                print("ffpmeg closed")
+                with open(self.path+'frame_times_%.2f.txt' % self.video_start_time, 'w') as fo:
+                    for t in self.video_times:
+                        fo.write('%f,%d,%d\n' % (t.secs+t.nsecs*1e-9, t.secs, t.nsecs))
+                print("frame times written")
+            
             subprocess.Popen(["sh","/home/dji/catkin_ws/src/rosbagg/src/bagend.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-	    self.writer.close()
-            print("ffpmeg closed")
-            with open(self.path+'frame_times_%.2f.txt' % self.video_start_time, 'w') as fo:
-                for t in self.video_times:
-                    fo.write('%f\n' % t)
             self.video_open = False
-            print("frame times written")
             return
+
         if self.video_open:
+
+            
+
             #print("image callback",msg.height, msg.width)
-            rostime = rospy.Time.now()
+            rostime = msg.header.stamp#rospy.Time.now()
+            t0 = TimerTime.time()
             im = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
-            #cv2.imshow("hiya", im)
-            #cv2.waitKey(0)
-            #cv2.imwrite("%04d.jpg" % self.frameid, im)
-            #self.frameid += 1
-            self.writer.write(im)
+            im = im[::2, ::2, :]
+                       
             time = rostime.secs + rostime.nsecs * 1e-9
-            self.video_times.append(time)
+ 
+            if self.image_mode:
+                cv2.imwrite(os.path.join(self.path, self.path_appendix,  str(time) + ".png"), im)
+            else:
+                print('\nrehsape took %f seconds' % (TimerTime.time() - t0))
+                #cv2.imshow("hiya", im)
+                #cv2.waitKey(0)
+                #cv2.imwrite("%04d.jpg" % self.frameid, im)
+                #self.frameid += 1
+                t0 = TimerTime.time()
+                self.writer.write(im)
+                print('writing took %f seconds\n' % (TimerTime.time() - t0))
+                self.video_times.append(rostime)
 
     # T -8000 (left)
     # P 8000  (center)
     # S 0     (right)
     def joy_cb(self, msg):
-	current_input = msg.axes[4]
+        current_input = msg.axes[4]
 
-	waiting = abs(rospy.get_time()-self.last_change_time) < self.delay_secs	
+        waiting = abs(rospy.get_time()-self.last_change_time) < self.delay_secs	
 
-	message = ""
+        message = ""
 
-	if current_input == -8000 and not self.close_video and not waiting:
+        if current_input == -8000 and not self.close_video and not waiting:
             self.close_video = True
-	    message = "close video command sent"
+            message = "close video command sent"
         elif current_input == 0 and self.close_video and not waiting:
             self.close_video = False
-	    message = "start video command sent"
-	elif not waiting:
-	    message = "accepting commands"
-	else:
-	    message = "waiting"
+            message = "start video command sent"
+        elif not waiting:
+            message = "accepting commands"
+        else:
+            message = "waiting"
 
-	if current_input != self.last_input:
-	    self.last_change_time = rospy.get_time() 
-	    message += "\nwaiting %.2f seconds before allowing next command" % self.delay_secs
+        if current_input != self.last_input:
+            self.last_change_time = rospy.get_time() 
+            message += "\nwaiting %.2f seconds before allowing next command" % self.delay_secs
 
-	if self.last_message != message:
-	   print(message)
-	   self.last_message = message
+        if self.last_message != message:
+           print(message)
+           self.last_message = message
 
-	self.last_input = current_input
+        self.last_input = current_input
 
 
 
