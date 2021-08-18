6f603fd5ce18839558ecded371d1731c8a59fe16
Ita Futran <icfutran@mit.edu>
2021-08-18 13:38:22 -0400

    fixed merge issue

--- dji_wrapper/include/dji_wrapper/dji_wrapper.hpp
+++ dji_wrapper/include/dji_wrapper/dji_wrapper.hpp
@@ -79,11 +79,7 @@ class DJIWrapper {
   void take_pic(const std_msgs::String &msg);
   void set_home(const std_msgs::String &msg);
   void follow_path(const nav_msgs::Path &msg);
-<<<<<<< HEAD
   void velocity_movement(const trajectory_msgs::JointTrajectory msg);
-=======
-  void follow_velocity_path(const nav_msgs::Path &msg);
->>>>>>> 351c5cf41141201b7cbff18f1cc7b7e0563b44b6
 
   void localFrameRefSubCallback(const sensor_msgs::NavSatFix::ConstPtr& localFrameRef);
   void timeSyncNmeaSubSCallback(const nmea_msgs::Sentence::ConstPtr& timeSyncNmeaMsg);
