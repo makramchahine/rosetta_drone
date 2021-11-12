8cd7c2d289d5acd1a5cad900d54bfbde7d43bca0
Ita Futran <icfutran@mit.edu>
2021-11-12 15:25:29 -0500

    hand-merged

--- rnn_control/src/rnn_control_node.py
+++ rnn_control/src/rnn_control_node.py
@@ -13,35 +13,14 @@ import tensorflow as tf
 from kerasncp.tf import LTCCell
 from sensor_msgs.msg import Image, Joy
 from tensorflow import keras
-import time
 
 # import logger from other ros package. Add to system path instead of including proper python lib dependency
 script_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(script_dir, "..", ".."))
 from video_compression.scripts.logger_example import Logger
 
-<<<<<<< HEAD
-def image_cb(msg):
-    global hidden_state, single_step_model
-    t0 = time.time()
-
-    im_np = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
-
-    im = PIL.Image.fromarray(im_np)
-    im_smaller = np.array(im.resize((256, 144), resample=PIL.Image.BILINEAR))
-    im_smaller = im_smaller / 255
-    im_smaller = im_smaller - mean
-    im_smaller = im_smaller / variance
-    im_smaller = np.expand_dims(im_smaller, 0)
-    print('downsize: %f' % (time.time() - t0))
-
-    # run inference on im_smaller
-    vel_cmd, hidden_state = single_step_model([im_smaller, hidden_state])
-
-=======
 
 def dji_msg_from_velocity(vel_cmd):
->>>>>>> 729f575a1c5746cf1dab9851313963eb127ae5be
     req = dji_srv.FlightTaskControlRequest()
     req.task = dji_srv.FlightTaskControlRequest.TASK_VELOCITY_AND_YAWRATE_CONTROL
     req.joystickCommand.x = vel_cmd[0][0]
@@ -49,78 +28,6 @@ def dji_msg_from_velocity(vel_cmd):
     req.joystickCommand.z = vel_cmd[0][2]
     req.joystickCommand.yaw = vel_cmd[0][3]
     req.velocityControlTimeMs = 40
-<<<<<<< HEAD
-
-    velocity_service.call(req)
-    print('full: %f' % (time.time() - t0))
-
-
-model_name = 'ncp'
-# make sure checkpoint includes script dir so script can be run from any file
-script_dir = os.path.dirname(os.path.abspath(__file__))
-checkpoint_name = 'rev-0_model-ncp_seq-64_opt-adam_lr-0.000900_crop-0.000000_epoch-020_val_loss:0.2127_mse:0.1679_2021:09:20:02:24:31'
-checkpoint_path = os.path.join(script_dir, checkpoint_name)
-
-last_model = tf.keras.models.load_model(checkpoint_path)
-weights_list = last_model.get_weights()
-
-IMAGE_SHAPE = (144, 256, 3)
-inputs = keras.Input(shape=IMAGE_SHAPE)
-
-rescaling_layer = keras.layers.experimental.preprocessing.Rescaling(1. / 255)
-# normalization layer unssupported by version of tensorflow on drone. Data instead normalized in callback
-mean = [0.41718618, 0.48529191, 0.38133072]
-variance = [.057, .05, .061]
-# normalization_layer = keras.layers.experimental.preprocessing.Normalization(mean=[0.41718618, 0.48529191, 0.38133072],
-#                                                                             variance=[.057, .05, .061])
-#normalization_layer = keras.layers.experimental.preprocessing.Normalization(axis=None)
-#normalization_layer.adapt(np.expand_dims((np.random.randn(*IMAGE_SHAPE) * np.sqrt(variance)) - mean, 0))
-#x = rescaling_layer(inputs)
-#x = normalization_layer(x)
-
-#x = keras.layers.Conv2D(filters=16, kernel_size=(5, 5), strides=(3, 3), activation='relu')(x)
-x = keras.layers.Conv2D(filters=16, kernel_size=(5, 5), strides=(3, 3), activation='relu')(inputs)
-x = keras.layers.Conv2D(filters=32, kernel_size=(3, 3), strides=(2, 2), activation='relu')(x)
-x = keras.layers.Conv2D(filters=64, kernel_size=(3, 3), strides=(2, 2), activation='relu')(x)
-x = keras.layers.Conv2D(filters=8, kernel_size=(3, 3), strides=(2, 2), activation='relu')(x)
-# fully connected layers
-x = keras.layers.Flatten()(x)
-x = keras.layers.Dense(units=128, activation='linear')(x)
-DROPOUT = 0.0
-pre_recurrent_layer = keras.layers.Dropout(rate=DROPOUT)(x)
-
-if model_name == 'ncp':
-    wiring = kncp.wirings.NCP(
-        inter_neurons=18,  # Number of inter neurons
-        command_neurons=12,  # Number of command neurons
-        motor_neurons=4,  # Number of motor neurons
-        sensory_fanout=6,  # How many outgoing synapses has each sensory neuron
-        inter_fanout=4,  # How many outgoing synapses has each inter neuron
-        recurrent_command_synapses=4,  # Now many recurrent synapses are in the
-        # command neuron layer
-        motor_fanin=6,  # How many incoming synapses has each motor neuron
-    )
-    rnn_cell = LTCCell(wiring)
-    inputs_state = tf.keras.Input(shape=(rnn_cell.state_size,))
-
-    motor_out, output_states = rnn_cell(pre_recurrent_layer, inputs_state)
-    single_step_model = tf.keras.Model([inputs, inputs_state], [motor_out, output_states])
-
-    # single_step_model.load_weights(checkpoint)
-    
-    single_step_model.set_weights(weights_list[3:])
-else:
-    raise ValueError(f"Illegal model name {model_name}")
-print('Loaded Model!!!')
-
-hidden_state = tf.zeros((1, rnn_cell.state_size))
-
-velocity_service = rospy.ServiceProxy('/flight_task_control', dji_srv.FlightTaskControl)
-rospy.Subscriber('dji_osdk_ros/main_camera_images', Image, image_cb)
-rospy.init_node("rnn_control")
-print("Finished initialization of model and ros setup")
-rospy.spin()
-=======
     return req
 
 
@@ -134,16 +41,9 @@ def load_model(model_name: str, checkpoint_name: str):
     IMAGE_SHAPE = (144, 256, 3)
     inputs = keras.Input(shape=IMAGE_SHAPE)
 
-    rescaling_layer = keras.layers.experimental.preprocessing.Rescaling(1. / 255)
     # normalization layer unssupported by version of tensorflow on drone. Data instead normalized in callback
-    mean = [0.41718618, 0.48529191, 0.38133072]
-    variance = [.057, .05, .061]
-    # normalization_layer = keras.layers.experimental.preprocessing.Normalization(mean=[0.41718618, 0.48529191, 0.38133072],
-    #                                                                             variance=[.057, .05, .061])
-    x = rescaling_layer(inputs)
-    # x = normalization_layer(x)
 
-    x = keras.layers.Conv2D(filters=16, kernel_size=(5, 5), strides=(3, 3), activation='relu')(x)
+    x = keras.layers.Conv2D(filters=16, kernel_size=(5, 5), strides=(3, 3), activation='relu')(inputs)
     x = keras.layers.Conv2D(filters=32, kernel_size=(3, 3), strides=(2, 2), activation='relu')(x)
     x = keras.layers.Conv2D(filters=64, kernel_size=(3, 3), strides=(2, 2), activation='relu')(x)
     x = keras.layers.Conv2D(filters=8, kernel_size=(3, 3), strides=(2, 2), activation='relu')(x)
@@ -196,9 +96,12 @@ class RNNControlNode:
         self.last_input = 8000
         # the last message printed by the state machine - used to reduce spam
         self.last_message = "starting up"
+
         print(f"Logging for this run: {log_data}")
 
-        # TODO: load model name and checkpoint from rosparam
+        # TODO: load model name and checkpoint, mean, var from rosparam
+        self.mean = [0.41718618, 0.48529191, 0.38133072]
+        self.variance = [.057, .05, .061]
         model_name = 'ncp'
         checkpoint_name = 'rev-0_model-ncp_seq-64_opt-adam_lr-0.000900_crop-0.000000_epoch-020_val_loss:0.2127_mse:0.1679_2021:09:20:02:24:31'
         self.single_step_model, rnn_cell = load_model(model_name, checkpoint_name)
@@ -217,7 +120,11 @@ class RNNControlNode:
         im_np = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
         # resize image
         im = PIL.Image.fromarray(im_np)
-        im_smaller = im.resize((256, 144), resample=PIL.Image.BILINEAR)
+        im_smaller = np.array(im.resize((256, 144), resample=PIL.Image.BILINEAR))
+        im_smaller = im_smaller / 255
+        im_smaller = im_smaller - self.mean
+        im_smaller = im_smaller / self.variance
+        im_smaller = np.expand_dims(im_smaller, 0)
         im_expanded = np.expand_dims(im_smaller, 0)
 
         if not self.video_open and not self.close_video:
@@ -303,4 +210,3 @@ if __name__ == "__main__":
     path_param = rospy.get_param("~path", default="/home/dji/flash/")
     log_daa = rospy.get_param("~log_data", default=True)
     node = RNNControlNode(path_param, log_daa)
->>>>>>> 729f575a1c5746cf1dab9851313963eb127ae5be
