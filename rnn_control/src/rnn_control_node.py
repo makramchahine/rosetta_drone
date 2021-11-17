0a23500c4ea0302b43ffa969e7cfa270155f9eb5
Patrick Kao <pdkao@mit.edu>
2021-11-18 02:16:25 +0800

    model

--- rnn_control/src/rnn_control_node.py
+++ rnn_control/src/rnn_control_node.py
@@ -298,6 +298,6 @@ class RNNControlNode:
 if __name__ == "__main__":
     path_param = rospy.get_param("~path", default="~/flash/")
     log_data = rospy.get_param("~log_data", default=False)
-    model_name = rospy.get_param("~model_name", default="ncp_old")
-    model_checkpoint = rospy.get_param("~checkpoint_path", default="models/ncp_old.hdf5")
+    model_name = rospy.get_param("~model_name", default="mixedcfc")
+    model_checkpoint = rospy.get_param("~checkpoint_path", default="models/mixedcfc1.hdf5")
     node = RNNControlNode(path_param, log_data, model_name, model_checkpoint)
